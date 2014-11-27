3866e899a1456ff4fcf9283da9279e2cd8ae722c
MessiKrkic <MessiKrkic@gmail.com>
2014-11-27 15:52:28 +0100

    made test for insertguests. The first one is fine, the second one throws an error.

--- server/model/domain/guestlayer.js
+++ server/model/domain/guestlayer.js
@@ -1,10 +1,14 @@
 var mongoose = require('mongoose');
 var guest = mongoose.model('Guest');
+var ObjectId = require('mongoose').Types.ObjectId;
 
 
 function insertGuests(guests, bId, callback) {
-    for(g in guests){
-        g.booking = bId;
+//    for(g in guests){
+//        g.booking = bId;
+//    }
+    for(var i = 0; i < guests.length; i++){
+        guests[i].booking = bId;
     }
     guest.create(guests, function (err, gs) {
         if(err) {
