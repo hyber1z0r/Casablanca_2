f1de8c3c411c03a12d60ac7c052910655da7b553
hyber1z0r <jakobsky@live.dk>
2014-11-27 19:11:22 +0100

    Changed insertGuest to return rowsinserted instead of guestarray

--- server/model/domain/guestlayer.js
+++ server/model/domain/guestlayer.js
@@ -1,20 +1,15 @@
 var mongoose = require('mongoose');
 var guest = mongoose.model('Guest');
-var ObjectId = require('mongoose').Types.ObjectId;
-
 
 function insertGuests(guests, bId, callback) {
-//    for(g in guests){
-//        g.booking = bId;
-//    }
-    for(var i = 0; i < guests.length; i++){
+    for (var i = 0; i < guests.length; i++) {
         guests[i].booking = bId;
     }
-    guest.create(guests, function (err, gs) {
-        if(err) {
+    guest.create(guests, function (err) {
+        if (err) {
             callback(err)
         } else {
-            callback(null, gs);
+            callback(null, arguments.length - 1);
         }
     });
 }
--- test/backend-tests/guestlayertest.js
+++ test/backend-tests/guestlayertest.js
@@ -3,54 +3,53 @@ global.TEST_DATABASE = "mongodb://localhost/TestDataBase_zz1243";
 var should = require("should");
 var app = require("../../server/app");
 var guestlayer = require('../../server/model/domain/guestlayer');
-var testdata = require('./testdb');
+var mongoose = require('mongoose');
+var guest = mongoose.model('Guest');
 var ObjectId = require('mongoose').Types.ObjectId;
 
 describe('GuestLayer', function () {
 
-    describe('insertGuests', function () {
+    beforeEach(function (done) {
+        guest.remove({}, function () {
+            done();
+        });
+    });
 
+    describe('insertGuests', function () {
         it('Should not return a error when a new guest is inserted', function (done) {
             var guestArray = [];
-            var toby = {firstName: "Toby", lastName: "Maguire", address: "Spiderman address",
-            phone: "88888888", email: "Toby@SpiderMail.com", dateOfBirth: "1982-07-23"};
+            var toby = {
+                firstName: "Toby", lastName: "Maguire", address: "Spiderman address",
+                phone: "88888888", email: "Toby@SpiderMail.com", dateOfBirth: "1982-07-23"
+            };
 
-            var dami = {firstName: "dami", lastName: "Maguire", address: "Spiderman address",
-            phone: "88888888", email: "dami@SpiderMail.com", dateOfBirth: "1982-07-23"};
+            var dami = {
+                firstName: "dami", lastName: "Maguire", address: "Spiderman address",
+                phone: "88888888", email: "dami@SpiderMail.com", dateOfBirth: "1982-07-23"
+            };
 
-            var rami = {firstName: "rami", lastName: "Maguire", address: "Spiderman address",
-            phone: "88888888", email: "rami@SpiderMail.com", dateOfBirth: "1982-07-23"};
+            var rami = {
+                firstName: "rami", lastName: "Maguire", address: "Spiderman address",
+                phone: "88888888", email: "rami@SpiderMail.com", dateOfBirth: "1982-07-23"
+            };
 
-            var sami = {firstName: "sami", lastName: "Maguire", address: "Spiderman address",
-            phone: "88888888", email: "sami@SpiderMail.com", dateOfBirth: "1982-07-23"};
+            var sami = {
+                firstName: "sami", lastName: "Maguire", address: "Spiderman address",
+                phone: "88888888", email: "sami@SpiderMail.com", dateOfBirth: "1982-07-23"
+            };
 
-            var lami = {firstName: "lami", lastName: "Maguire", address: "Spiderman address",
-            phone: "88888888", email: "lami@SpiderMail.com", dateOfBirth: "1982-07-23"};
+            var lami = {
+                firstName: "lami", lastName: "Maguire", address: "Spiderman address",
+                phone: "88888888", email: "lami@SpiderMail.com", dateOfBirth: "1982-07-23"
+            };
 
             guestArray.push(toby, dami, rami, sami, lami);
             var bId = new ObjectId(202);
-            guestlayer.insertGuests(guestArray, bId, function (err, documents) {
+            guestlayer.insertGuests(guestArray, bId, function (err, rowsInserted) {
                 should.not.exist(err);
+                rowsInserted.should.equal(guestArray.length);
                 done();
             });
         });
-        it('Should have all the right properties', function (done) {
-            var guestArray = [];
-            var toby = {firstName: "Toby", lastName: "Maguire", address: "Spiderman address",
-                phone: "88888888", email: "Toby@SpiderMail.com", dateOfBirth: "1983-07-23"};
-            var bId = new ObjectId();
-            guestArray.push(toby);
-            guestlayer.insertGuests(guestArray, bId, function (err, document) {
-                document.should.have.property('firstName');
-                document.should.have.property('lastName');
-                document.should.have.property('addressName');
-                document.should.have.property('phone');
-                document.should.have.property('email');
-                document.should.have.property('dateOfBirth');
-                document.should.have.property('booking')
-                done();
-            });
-        });
-
     });
 });
