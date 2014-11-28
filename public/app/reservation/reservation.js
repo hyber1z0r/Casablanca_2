5a1380e4f496763009f6c8fdf214c9b9fa3ef041
hyber1z0r <jakobsky@live.dk>
2014-11-28 09:23:29 +0100

    Got search to work with factory

--- public/app/reservation/reservation.js
+++ public/app/reservation/reservation.js
@@ -12,8 +12,17 @@ angular.module('casablanca.reservation', ['ngRoute'])
     .controller('ReservationCtrl', function ($scope, hotelBookingFactory) {
         $scope.search = function () {
             hotelBookingFactory.getFreeRooms($scope.startDate, $scope.endDate, $scope.roomsize, function (err, data) {
+                if (err) {
 
+                } else if (data.length == 0){
+                    console.log(data);
+                    console.log('No free rooms?');
+
+                } else {
+                    var room_id = data[0]._id;
+                    console.log(room_id);
+                    $scope.showRest = true
+                }
             });
-            $scope.showRest = true
         }
     });
