a895c86ccf56e1a18e2598e9a514f99d8e28dc33
MessiKrkic <MessiKrkic@gmail.com>
2014-12-09 14:37:00 +0100

    testdb + sampledata for facilitys

--- test/backend-tests/testdb.js
+++ test/backend-tests/testdb.js
@@ -2,9 +2,14 @@ var mongoose = require("mongoose");
 var booking = mongoose.model("Booking");
 var room = mongoose.model("Room");
 var guest = mongoose.model('Guest');
+var facility = mongoose.model('Facility');
+var facilitybooking = mongoose.model('FacilityBooking');
 var bookingArray = require('../../server/model/sampledata/bookings.json').slice(0, 11);
 var roomArray = require('../../server/model/sampledata/rooms.json').slice(0, 11);
 var guestArray = require('../../server/model/sampledata/guests.json').slice(0, 11);
+var facilityArray = require('../../server/model/sampledata/guests.json').slice(0, 11);
+var facilityBookingArray = require('../../server/model/sampledata/guests.json').slice(0, 11);
+
 
 function insertBookings(done) {
     booking.remove({}, function () {
@@ -30,6 +35,24 @@ function insertGuests(done) {
     })
 }
 
+function insertFacilitys(done) {
+    facility.remove({}, function () {
+        facility.create(facilityArray, function (err) {
+            done();
+        })
+    })
+}
+
+function insertFacilityBookings(done) {
+    facilitybooking.remove({}, function () {
+        facilitybooking.create(facilityBookingArray, function (err) {
+            done();
+        })
+    })
+}
+
 module.exports.insertBookings = insertBookings;
 module.exports.insertRooms = insertRooms;
 module.exports.insertGuests = insertGuests;
+module.exports.insertFacilitys = insertFacilitys;
+module.exports.insertFacilityBookings = insertFacilityBookings;
