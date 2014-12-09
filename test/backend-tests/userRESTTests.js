b6197a21e5d5a02acc3d6b3bdfdb6cc3d03e3a14
hyber1z0r <jakobsky@live.dk>
2014-12-09 11:23:58 +0100

    Added more teststo userrest

--- test/backend-tests/userRESTTests.js
+++ test/backend-tests/userRESTTests.js
@@ -9,43 +9,78 @@ var mongoose = require('mongoose');
 var supertest = require('supertest');
 var testdata = require('./testdb');
 var bookinglayer = require('../../server/model/domain/bookinglayer');
+var ObjectId = require('mongoose').Types.ObjectId;
 
 
 
 describe('userREST API for /userapi', function () {
 
 
-   beforeEach(function (done) {
-      testdata.insertRooms(function () {
-         testdata.insertGuests(function () {
-            testdata.insertBookings(function () {
-               done();
+    beforeEach(function (done) {
+        testdata.insertRooms(function () {
+            testdata.insertGuests(function () {
+                testdata.insertBookings(function () {
+                    done();
+                });
             });
-         });
-      });
-   });
+        });
+    });
 
-   it('Should return one booking', function (done) {
-      var start = new Date();
-      var slut = new Date();
-      slut.setFullYear(2014, 11, 10);
-      var room = 1;
-      bookinglayer.insertBooking(start, slut, room, function (err, booking) {
-         supertest(app)
-             .get('/userApi/getBooking/' + booking._id)
-             .end(function (err, res) {
-                should.not.exist(err);
-                var b = JSON.parse(res.text);
-                b._id.toString().should.equal(booking._id.toString());
-                b.should.have.property('startDate');
-                b.should.have.property('endDate');
-                b.should.have.property('room');
-                b.should.have.property('regDate');
-                done();
-             });
-      });
+    describe('/getBooking', function () {
+        it('Should return one booking', function (done) {
+            var start = new Date();
+            var slut = new Date();
+            slut.setFullYear(2014, 11, 10);
+            var room = 1;
+            bookinglayer.insertBooking(start, slut, room, function (err, booking) {
+                supertest(app)
+                    .get('/userApi/getBooking/' + booking._id)
+                    .end(function (err, res) {
+                        should.not.exist(err);
+                        var b = JSON.parse(res.text);
+                        b._id.toString().should.equal(booking._id.toString());
+                        b.should.have.property('startDate');
+                        b.should.have.property('endDate');
+                        b.should.have.property('room');
+                        b.should.have.property('regDate');
+                        done();
+                    });
+            });
+        });
 
+        it('Should return null, when not found/invalid id', function (done) {
+            // invalid made up objectID:
+            var bookingID = new ObjectId("123db2df28fcd1341d67fa99");
+            supertest(app)
+                .get('/userApi/getBooking/' + bookingID)
+                .end(function (err, res) {
+                    should.not.exist(err);
+                    var result = JSON.parse(res.text);
+                    (result === null).should.equal(true);
+                    done();
+                });
+        });
+    });
 
-   });
+    describe('/findGuest', function () {
 
+    });
+
+    describe('/getGuests', function () {
+
+    });
+
+    describe('/deleteGuests', function () {
+
+    });
+
+    describe('/deleteLogin', function () {
+
+    });
+
+    describe('/getFreeTimes', function () {
+        it('Should return X free times', function (done) {
+            done();
+        })
+    });
 });
