e3dc94cf7605743845066af981633f76ea3efd11
hyber1z0r <jakobsky@live.dk>
2014-12-10 12:32:10 +0100

    Azure

--- server/model/domain/guestlayer.js
+++ server/model/domain/guestlayer.js
@@ -41,7 +41,7 @@ function findGuest(gId, callback) {
 /* Posts the username and password, with their obj ID, to the JPA Server -> Oracle DB */
 function insertUsernames(gs, callback) {
     request.post({
-        url: 'http://http://ohnana.cloudapp.net/',
+        url: 'http://http://ohnana.cloudapp.net/user',
         body: gs,
         json: true
     }, function optionalCallback(err, httpResponse, body) {
@@ -80,7 +80,7 @@ function deleteBooking(id, callback) {
 
 function deleteLogin(username, callback) {
     console.log('Calling request delete:');
-    request.del('http://http://ohnana.cloudapp.net/' + username);
+    request.del('http://http://ohnana.cloudapp.net/user' + username);
     callback(null, 'Wuhu');
 }
 
